05d34724f81995d69867e49f41e5e8ebae663b49
Sihan Tao JMC UG <sihan.tao20@imperial.ac.uk>
2022-06-08 00:22:12 +0100

    Press block redirect to website

--- components/AutomatedSlideShow.js
+++ components/AutomatedSlideShow.js
@@ -3,34 +3,36 @@ import { Component } from "react";
 
 export default class AutomatedSlideshow extends Component {
     constructor(props) {
-      super(props);
-   
-      this.state = {
-        position: 1,
-        interval: null,
-      };
+        super(props);
+
+        this.state = {
+            position: 1,
+            interval: null,
+        };
     }
-   
+
     componentWillMount() {
-      this.setState({
-        interval: setInterval(() => {
-          this.setState({
-            position: this.state.position === this.props.dataSource.length ? 0 : this.state.position + 1
-          });
-        }, 2000)
-      });
+        this.setState({
+            interval: setInterval(() => {
+                this.setState({
+                    position: this.state.position === this.props.dataSource.length ? 0 : this.state.position + 1
+                });
+            }, 2000)
+        });
     }
-   
+
     componentWillUnmount() {
-      clearInterval(this.state.interval);
+        clearInterval(this.state.interval);
     }
-   
+
     render() {
-      return (
-      <Slideshow 
-          dataSource={this.props.dataSource}
-          position={this.state.position}
-          onPositionChanged={position => this.setState({ position })} />
-      );
+        return (
+            <Slideshow
+                onPress={this.props.onPress}
+                titleStyle={this.props.style}
+                dataSource={this.props.dataSource}
+                position={this.state.position}
+                onPositionChanged={position => this.setState({ position })} />
+        );
     }
-  }
+}
