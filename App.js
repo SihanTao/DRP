60de6347451f04eda3ad170dc3402f3b72b08aab
Sihan Tao JMC UG <sihan.tao20@imperial.ac.uk>
2022-06-09 17:15:52 +0100

    Change back to firebase 9.6.1

--- App.js
+++ App.js
@@ -12,6 +12,22 @@ enableScreens();
 
 import Screens from "./navigation/Screens";
 import { Images, articles, argonTheme } from "./constants";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrHlxTMFpSJpK9wOwc00-0FgNmQWU4uFI",
+  authDomain: "drp22-c4cf7.firebaseapp.com",
+  databaseURL: "https://drp22-c4cf7-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "drp22-c4cf7",
+  storageBucket: "drp22-c4cf7.appspot.com",
+  messagingSenderId: "641039613250",
+  appId: "1:641039613250:web:120f3ee1b49e0c4727dcdb",
+  measurementId: "G-RCTVFJVMC5"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 // cache app images
 const assetImages = [
