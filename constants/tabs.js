bdf8c2b7ab942d4fc98b7f5406a01c031215a76f
Sihan Tao JMC UG <sihan.tao20@imperial.ac.uk>
2022-06-10 14:15:05 +0100

    Add a tab 'new facility'

--- constants/tabs.js
+++ constants/tabs.js
@@ -1,15 +1,10 @@
 export default tabs = {
-  // categories: [
-  //   { id: 'popular', title: 'Popular' },
-  //   { id: 'beauty', title: 'Beauty' },
-  //   { id: 'fashion', title: 'Fashion' },
-  //   { id: 'car_motorcycle', title: 'Car & Motorcycle' },
-  // ],
   categories: [
     {id: 'study_space', title: '#study_space'},
     {id: 'water_fountain', title: '#water_fountain'},
     {id: 'cafe', title: '#cafe'},
     {id: 'toilet', title: '#toilet'},
     {id: 'microwave', title: '#microwave'},
+    {id: 'new_facility', title: '#new_facility'}
   ]
 }
