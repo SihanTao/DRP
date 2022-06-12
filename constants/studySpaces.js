b6dc27bcdf9963f190e363308e728591147e021b
Sihan Tao JMC UG <sihan.tao20@imperial.ac.uk>
2022-06-12 18:32:57 +0100

    Add some mock study spaces.

---
+++ constants/studySpaces.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        name: 'Central Library Group Study',
+        category: 'Group Study',
+        description: 'Level 1',
+        photo: '',
+        rating: 4,
+    }, {
+        name: 'Central Library Quiet Study',
+        category: 'Quiet Study',
+        description: 'Levels 1,4,5',
+        photo:'',
+        rating: 3,
+    }, {
+        name: 'Central Library Silent Study',
+        category: 'Silent Study',
+        description: 'Levels 2 and 3',
+        rating: 5,
+    }
+];
