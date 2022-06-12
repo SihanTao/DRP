3a356f6434f2cec79151ebbe860974d34b9ee653
Sihan Tao JMC UG <sihan.tao20@imperial.ac.uk>
2022-06-12 13:22:07 +0100

    Firebase google signin not working.

--- backend/auth.js
+++ backend/auth.js
@@ -1,5 +1,41 @@
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    GoogleAuthProvider,
+    signInWithRedirect
+} from "firebase/auth";
 
+
+// Google SignIn
+
+// export function googleSignIn() {
+//     const provider = new GoogleAuthProvider();
+//     const auth = getAuth();
+//     signInWithRedirect(auth, provider)
+//         .catch((error) => {
+//             // Handle Errors here.
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//             // The email of the user's account used.
+//             const email = error.customData.email;
+//             // The AuthCredential type that was used.
+//             const credential = GoogleAuthProvider.credentialFromError(error);
+//             // ...
+//         });
+// }
+
+// export function googleSignOut() {
+//     const auth = getAuth();
+//     signOut(auth).then(() => {
+//         // Sign-out successful.
+//     }).catch((error) => {
+//         // An error happened.
+//     });
+// }
+
+// Traditional Email & Password sign in 
 export function createUserWithEmail(useremail, userpassword) {
     const auth = getAuth();
 
@@ -34,9 +70,9 @@ export function signInWithEmail(email, password) {
 export function signOutWrapper() {
     const auth = getAuth();
     signOut(auth).then(() => {
-      // Sign-out successful.
+        // Sign-out successful.
     }).catch((error) => {
-      // An error happened.
+        // An error happened.
     });
 }
 
