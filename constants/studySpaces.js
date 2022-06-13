0ff54dd3fd6a101f4dc2018a67b056b40d580812
Sihan Tao JMC UG <sihan.tao20@imperial.ac.uk>
2022-06-13 06:13:09 +0100

    Add Gostudy data in firebase database.

--- constants/studySpaces.js
+++ constants/studySpaces.js
@@ -1,20 +1,41 @@
+const SILENTSTUDY = 'Silent Study';
+const GROUPSTUDY = 'Group Study';
+const QUIETSTUDY = 'Quiet Study';
+
 const studySpaces = [
     {
         name: 'Central Library Group Study',
-        category: 'Group Study',
+        category: {GROUPSTUDY},
         description: 'Level 1',
         photo: '',
         rating: 4,
+        numRatings: 0, 
     }, {
         name: 'Central Library Quiet Study',
-        category: 'Quiet Study',
+        category: {QUIETSTUDY},
         description: 'Levels 1,4,5',
         photo:'',
+        numRatings: 0, 
         rating: 3,
     }, {
         name: 'Central Library Silent Study',
-        category: 'Silent Study',
+        category: {SILENTSTUDY},
         description: 'Levels 2 and 3',
+        numRatings: 0, 
+        rating: 5,
+    }, {
+        name: 'GoStudy Silent Study',
+        category: {SILENTSTUDY},
+        description: 'Chemistry Building Rooms 460, 537',
+        numRatings: 0, 
+        rating: 5,
+    }, {
+        name: 'GoStudy Group Study',
+        category: {GROUPSTUDY},
+        description: 'Chemistry Building Rooms 430, 444, 538, 561',
+        numRatings: 0, 
         rating: 5,
     }
-]
+];
+
+export default studySpaces;
