f3ececc0134a34ddeeac14731d9886c96c9460a7
JarvisYu <JarvisYu_Pro@outlook.com>
2022-06-14 20:46:57 +0100

    Makes YetToBeImplementedScreen has guide to turn it off

--- constants/DevelopeStatus.js
+++ constants/DevelopeStatus.js
@@ -1,11 +1,20 @@
 import { Text } from "react-native-elements";
+import { StyleSheet } from "react-native";
 import DEV_STATUS from "./DevStatus";
 import { Block } from "galio-framework";
 
 function YetToBeImplementedScreen () {
   return (
-    <Block center style={[{flex: 1}, {flexDirection: "row"}, {alignItems: "center"}]}>
-      <Text> YetToBeImplementedScreen </Text>
+    <Block center style={styles.block}>
+      <Block>
+        <Text style={styles.bigText}>
+          This feature is yet to be implemented.
+        </Text>
+        <Text style={styles.smallText}>
+          If you are a developer for the project. You might want to set
+          DEV_STATUS value to "developing" in "constants/DevStatus.js"
+        </Text>
+      </Block>
     </Block>
   )
 }
@@ -28,3 +37,18 @@ export default function DevStatus ({status, children}) {
     {children}
   </>)
 }
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  bigText: {
+    fontSize: 26,
+    fontWeight: "bold"
+  },
+  smallText: {
+    fontSize: 12,
+  },
+})
