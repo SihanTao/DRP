2a77b6e84fe01160b83c295e0dec36f1bca5a7ad
Sihan Tao JMC UG <sihan.tao20@imperial.ac.uk>
2022-06-14 09:38:33 +0100

    Can print activetags on the screen.

--- screens/SearchResult.js
+++ screens/SearchResult.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Text } from 'galio-framework';
-import { View, TextInput, StyleSheet, ImageBackground, Dimensions, Linking } from "react-native";
+import { View, TextInput, StyleSheet, ImageBackground, Dimensions, Linking, FlatList } from "react-native";
 import { ColorPicker, ModalInput, Separator, Tag } from "react-native-btr";
 import DropDownSearchBar from "../components/DropDownSearchBar";
 import SearchBarWithTag from "../components/SearchBarWithTag";
@@ -16,23 +16,17 @@ const { width } = Dimensions.get('screen');
 
 export default function SearchResult() {
   let [tags, setTags] = useState([
-    { name: "study space", color: "#f43", active: false },
+    { name: "study space", color: "#f43", active: true },
     { name: "silent", color: "#484", active: false },
     { name: "group", color: "#E91", active: false },
-    { name: "# toilet", color: "#9C2", active: false },
-    { name: "# microwave", color: "#673", active: false },
-    { name: "# water_fountain", color: "#4CA", active: false },
+    { name: "quiet", color: "#9C2", active: false },
+    // { name: "# microwave", color: "#673", active: false },
+    // { name: "# water_fountain", color: "#4CA", active: false },
   ]);
 
   const [visible, setVisible] = useState(false);
   const [color, setColor] = useState("");
   const [text, setText] = useState("");
-
-  const removeTag = (index) => {
-    const tag = tags[index];
-    tag.active = !tag.active;
-    setTags([...tags]);
-  };
 
   const toggleTag = (index) => {
     const tag = tags[index];
@@ -40,102 +34,52 @@ export default function SearchResult() {
     setTags([...tags]);
   };
 
-  const toggle = () => setVisible((visible) => !visible);
+  function renderActiveTags(tag) {
+    if (tag.active) {
+      return (
+        <Text>{tag.name}</Text>
+      )
+    }
+  }
 
-  const onCancel = () => {
-    setColor("");
-    setText("");
-    toggle();
-  };
-
-  const onOk = () => {
-    tags = [...tags, { name: text, color, active: false }];
-    setTags(tags);
-    onCancel();
-  };
 
   return (
-
     <View style={styles.container}>
-      <View style={styles.container2}>
-
-        {/* <SearchBarWithTag /> */}
-        <View
-          style={{ flexDirection: "row", flexWrap: "wrap" }}
-          iconContent={<IconExtra size={16} color={theme.COLORS.MUTED} name="search-zoom-in" family="ArgonExtra" />}
-        >
-          {tags
-            .filter((tag) => tag.active)
-            .map((tag, index) => (
+      <View style={styles.container3}>
+        <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+          {tags.map((tag, index) => {
+            const backgroundColor = tag.active ? tag.color : "#0000";
+            const color = tag.active ? "#fff" : tag.color;
+            return (
               <View key={tag.name + index} style={{ margin: 2 }}>
                 <Tag
                   name={tag.name}
                   style={{
-                    backgroundColor: tag.color,
-                    color: "#fff",
+                    backgroundColor,
+                    color,
+                    borderWidth: 1,
                     borderRadius: 50,
                   }}
-                  iconRight="cancel"
-                  onPress={() => {removeTag(index)}}
-                />
+                  onPress={() => toggleTag(index)} />
               </View>
-            ))}
+            );
+          })}
         </View>
+
       </View>
-      <View style={styles.container5}>
-        <View style={styles.container3}>
-          <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-            {tags.map((tag, index) => {
-              const backgroundColor = tag.active ? tag.color : "#0000";
-              const color = tag.active ? "#fff" : tag.color;
-              return (
-                <View key={tag.name + index} style={{ margin: 2 }}>
-                  <Tag
-                    name={tag.name}
-                    style={{
-                      backgroundColor,
-                      color,
-                      borderWidth: 1,
-                      borderRadius: 50,
-                    }}
-                    onPress={() => toggleTag(index)}
-                  />
-                </View>
-              );
-            })}
-          </View>
-          {visible && (
-            <ModalInput onCancel={onCancel} onOk={onOk}>
-              <ColorPicker
-                selectedColor={color}
-                onSelect={(color) => setColor(color)}
-              />
-              <Separator />
-              <TextInput
-                autoFocus={true}
-                placeholder="Tag"
-                multiline={true}
-                value={text}
-                onChangeText={(text) => setText(text)}
-                underlineColorAndroid="#0000"
-                style={styles.textInput}
-              />
-            </ModalInput>
-          )}
-        </View>
-        <View style={styles.container5}>
-          <Home noRecommendation={true}></Home>
-        </View>
-      </View>
+      <FlatList
+        data={tags}
+        renderItem={({ item }) => renderActiveTags(item)}
+      />
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     marginTop: 0,
     width: '100%',
     //backgroundColor: "#abdbe3",
