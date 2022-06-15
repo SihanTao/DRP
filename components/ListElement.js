11789e1a7879eba9ed12fbf3ec2c607a805c73e8
Sihan Tao JMC UG <sihan.tao20@imperial.ac.uk>
2022-06-15 17:31:50 +0100

    Change the opacity of rgba.

--- components/ListElement.js
+++ components/ListElement.js
@@ -92,7 +92,7 @@ const styles = StyleSheet.create({
   categoryTitle: {
     height: "100%",
     paddingHorizontal: theme.SIZES.BASE,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    backgroundColor: "rgba(0, 0, 0, 0.3)",
     justifyContent: "center",
     alignItems: "center",
   },
