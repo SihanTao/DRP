3a6d1340c9ba9114b5401348cfa2aba57812598d
JarvisYu <JarvisYu_Pro@outlook.com>
2022-06-16 15:05:45 +0100

    Adds maps for all facilities with some placeholders

--- constants/facilities.js
+++ constants/facilities.js
@@ -12,6 +12,12 @@ const facilities = [
             'Huxley ground floor, inside computing lab, need to access lab\n' +
             'Go straight for 20 meters',
         photo: 'https://i.ibb.co/LQtJYJM/computing-lab-water.jpg',
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
         url: '',
         location: 'Huxley ground-floor computing lab',
         openingHour: '',
@@ -28,6 +34,12 @@ const facilities = [
         description:
             'Inside Senior common room, next to the food-selling place\n',
         photo: 'https://i.ibb.co/dLk9zCj/Cafe-water.jpg',
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
         url: '',
         location: 'Senior common room',
         openingHour: '',
@@ -45,6 +57,12 @@ const facilities = [
             'Sherfield 2nd floor, next to the accessible toilets\n' +
             'There are 2 of them both next to accessible toilets',
         photo: 'https://i.ibb.co/Kznwhmb/sherfield-2-water.jpg',
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
         url: '',
         location: 'Sherfield 2nd floor',
         openingHour: '',
@@ -67,6 +85,12 @@ const facilities = [
             'Group study - level 1\n' +
             'Height adjustable desks - levels 2 and 3\n',
         photo: 'https://www.imperial.ac.uk/media/migration/administration-and-support-services/0000-191111-autumn-library-queens-lawn-018_1635242587445_x2.jpg',
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
         url: 'https://www.imperial.ac.uk/admin-services/library/use-the-library/our-libraries/central-library/',
         location: '',
         openingHour: '',
@@ -84,6 +108,12 @@ const facilities = [
             'Silent Study: Chemistry Building Rooms 460, 537\n' +
             'Group Study: Chemistry Building Rooms 430, 444, 538, 561',
         photo: 'https://pxl-imperialacuk.terminalfour.net/fit-in/959x430/prod01/channel_2/media/migration/administration-and-support-services/211029-gostudy-spaces-rm537-001_1635779844596_x4.jpg',
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
         url: 'https://www.imperial.ac.uk/admin-services/library/use-the-library/our-libraries/gostudy/',
         location: '',
         openingHour: '',
@@ -101,6 +131,11 @@ const facilities = [
         rating: 0,
         numRatings: 0,
         photo: 'https://www.imperial.ac.uk/ImageCropToolT4/imageTool/uploaded-images/IMG_20151110_122250365--tojpeg_1447160413584_x2.jpg',
+        maps: [
+            {
+                url: 'https://i.ibb.co/d6DmFFT/Huxley-L2-213.png'
+            }
+        ],
         location: '',
         openingHour: '',
         url: 'https://www.imperial.ac.uk/computing/csg/facilities/lab/'
@@ -116,6 +151,11 @@ const facilities = [
         rating: 0,
         numRatings: 0,
         photo: 'https://i.ibb.co/HTjWccS/341-342.jpg',
+        maps: [
+            {
+                url: 'https://i.ibb.co/6P0DLhM/Huxley-L3-342-341.png'
+            },
+        ],
         location: '',
         openingHour: '',
         url: ''
@@ -131,6 +171,11 @@ const facilities = [
         rating: 0,
         numRatings: 0,
         photo: 'https://i.ibb.co/M2F716f/212.jpg',
+        maps: [
+            {
+                url: 'https://i.ibb.co/vQND1Vz/Huxley-L2-212.png'
+            },
+        ],
         location: '',
         openingHour: '',
         url: ''
@@ -149,6 +194,11 @@ const facilities = [
         rating: 0,
         numRatings: 0,
         photo: 'https://i.ibb.co/MnY9Fx3/227.jpg',
+        maps: [
+            {
+                url: 'https://i.ibb.co/7pmt2dh/Huxley-L2-227.png'
+            }
+        ],
         location: '',
         openingHour: '',
         url: ''
@@ -164,6 +214,11 @@ const facilities = [
         rating: 0,
         numRatings: 0,
         photo: 'https://i.ibb.co/SPxzrdj/224.jpg',
+        maps: [
+            {
+                url: 'https://i.ibb.co/VMVwpJL/Huxley-L2-224.png'
+            }
+        ],
         location: '',
         openingHour: '',
         url: ''
@@ -176,6 +231,12 @@ const facilities = [
             FEMALE: true,
         },
         photo: 'https://w7.pngwing.com/pngs/310/99/png-transparent-unisex-public-toilet-bathroom-fee-s-text-bathroom-logo.png',
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
         description: "Library Cafe",
         location: 'Library Cafe'
     }, {
@@ -187,9 +248,14 @@ const facilities = [
             FEMALE: true, 
         },
         photo: 'https://i.ibb.co/FzVmGZG/20220615221920.jpg',
+        maps: [
+            {
+                url: 'https://i.ibb.co/GczsmMm/Huxley-L2-236-C.png'
+            }
+        ],
         description: "Huxley Building 236C..." 
     }, {
-        name: 'Secret Luxurious Male toilet',
+        name: 'Secret Luxurious Male Toilet',
         toilet: true,
         TOILET: {
             ACCESSIBLE: false,
@@ -197,9 +263,14 @@ const facilities = [
             FEMALE: false, 
         },
         photo: 'https://i.ibb.co/Fg3B122/secret-male.jpg',
+        maps: [
+            {
+                url: 'https://i.ibb.co/Tmz1vhL/Huxley-L2-Unamed-Toilets.png'
+            }
+        ],
         description: "Between Huxley labs and Blackett" 
     }, {
-        name: 'Secret Luxurious Female toilet',
+        name: 'Secret Luxurious Female Toilet',
         toilet: true,
         TOILET: {
             ACCESSIBLE: false,
@@ -207,9 +278,14 @@ const facilities = [
             FEMALE: true, 
         },
         photo: 'https://i.ibb.co/fqwDRMJ/secret-female.jpg',
+        maps: [
+            {
+                url: 'https://i.ibb.co/Tmz1vhL/Huxley-L2-Unamed-Toilets.png'
+            }
+        ],
         description: "Between Huxley labs and Blackett" 
     }, {
-        name: 'Secret Luxurious Accessible toilet',
+        name: 'Secret Luxurious Accessible Toilet',
         toilet: true,
         TOILET: {
             ACCESSIBLE: true,
@@ -217,6 +293,11 @@ const facilities = [
             FEMALE: true, 
         },
         photo: 'https://i.ibb.co/TTRtNw1/secret-accessible.jpg',
+        maps: [
+            {
+                url: 'https://i.ibb.co/Tmz1vhL/Huxley-L2-Unamed-Toilets.png'
+            }
+        ],
         description: "Between Huxley labs and Blackett" 
     },   
 
@@ -234,6 +315,12 @@ const facilities = [
         location: "The Senior Common Room is located on level 2 of the Sherfield Building.",
         description: 'The SCR Restaurant offers a complete menu of fresh hot and cold lunches every weekday. Our chefs prepare a variety of freshly-made hot meals including street food, vegan and vegetarian options, a salad bar, fresh soups, carvery roasts and a delicious selection of desserts. ',
         photo: 'https://pxl-imperialacuk.terminalfour.net/prod01/channel_2/media/migration/administration-and-support-services/IMG_20191001_112216-2--tojpeg_1578322000843_x4.jpg',
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
     }, {
         name: 'The Loud Bird',
         cafe: true,
@@ -249,6 +336,12 @@ const facilities = [
             "The Loud Bird offers a selection of grilled chicken, burgers, wraps, vegan options, tasty sides and signature sauces." +
             "The outlet is also in partnership with 'Roastology', serving hot coffee and tea all day.",
         photo: 'https://pxl-imperialacuk.terminalfour.net/prod01/channel_2/media/migration/administration-and-support-services/the-loud-bird-wood-backgroundpng_1613099809796_x4.jpg',
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
     }, {
         name: 'College Cafe',
         cafe: true,
@@ -266,6 +359,12 @@ const facilities = [
             "For lunch, why not try one of our trademark delicious hand carved roast sandwiches, made with a freshly prepared meat or fish filling and accompanied with a homemade dressing. " +
             "College Café also boasts a juice bar where you can order healthy juice concoctions and smoothies bursting with delicious, fresh flavours. Try one of the tried and tested favourites or create your own blend. Look out for the smoothie and juice of the day.",
         photo: "https://pxl-imperialacuk.terminalfour.net/prod01/channel_2/media/migration/administration-and-support-services/College_Cafe004--tojpeg_1440508268653_x4.jpg",
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
     }, {
         name: 'Library Cafe',
         cafe: true,
@@ -279,6 +378,12 @@ const facilities = [
         location: "The Library Café is located on the ground floor of the Imperial College Library Building, which faces onto the Queen's Lawn",
         description: "Whether you are looking for a quick pastry for breakfast, a filling baked potato for lunch, or simply a warm beverage and a space to study, the Library Café is a great choice morning, afternoon or night. With longer opening hours than many of our other outlets, the Library Café is there whenever you need to refuel after a hard day's work. Boasting comfortable booths and PCs connected to the College network it also makes an ideal place to study.",
         photo: "https://pxl-imperialacuk.terminalfour.net/prod01/channel_2/media/migration/administration-and-support-services/img-1493_1643298046507_x4.jpg",
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
     }, {
         name: 'Neo Pizza & Pasta',
         cafe: true,
@@ -292,6 +397,12 @@ const facilities = [
         location: "Neo Pizza and Pasta can be found in the JCR.",
         description: "Try one of our freshly prepared pizzas using only the very best ingredients.",
         photo: "https://pxl-imperialacuk.terminalfour.net/prod01/channel_2/media/migration/administration-and-support-services/vecteezy-four-pizza-on-a-dark-wooden-background-807105_1620134120021_x4.jpg",
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
     }, 
     // microwave 
     {
@@ -300,12 +411,24 @@ const facilities = [
         location: "Huxley 223. Inside DoC Lab Area",
         description: "Fridge, Coffee machine & microwave!",
         photo: "https://i.ibb.co/H2sxYnp/Do-CStudent-Tea-Point.jpg",
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
     }, {
         name: 'GoStudy Space Kitchen', 
         microwave: true,
         location: "Chemistry Building 448.",
         description: "GoStudy is open to all students and offers a variety of study environments to suit your needs including Silent Study, Group Study and Breakout Spaces where you can eat and drink.",
         photo: "https://pxl-imperialacuk.terminalfour.net/fit-in/959x430/prod01/channel_2/media/migration/administration-and-support-services/gostudy8_1635780402942_x4.jpg",
+        maps: [  // TODO: Add corrent map
+            {
+                url: 'https://ih1.redbubble.net/image.959951874.3860/pp,840x' +
+                     '830-pad,1000x1000,f8f8f8.jpg'
+            }
+        ],
     }
 ];
 
