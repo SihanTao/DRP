c9b486a0b13e2350e44d2be814ff8e9b4c104a0f
JarvisYu <JarvisYu_Pro@outlook.com>
2022-06-16 23:37:41 +0100

    Wraps favourites and profile into DevStatus

--- constants/DevStatus.js
+++ constants/DevStatus.js
@@ -2,6 +2,6 @@
    - "developing": Currently developing
    - "publishing": Hide unfinished features
 */
-const DEV_STATUS = "developing"
-// const DEV_STATUS = "publishing"
+// const DEV_STATUS = "developing"
+const DEV_STATUS = "publishing"
 export default DEV_STATUS
