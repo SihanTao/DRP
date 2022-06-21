59a582bb4a93ff1a5668fe86d166a2b1ccf7d1ad
JarvisYu <JarvisYu_Pro@outlook.com>
2022-06-21 18:51:06 +0100

    Applies react-hook-form to WikiShare page

--- screens/WikiShare.js
+++ screens/WikiShare.js
@@ -6,51 +6,86 @@ import { Button } from "react-native-elements";
 import { StyleSheet, Dimensions, Keyboard, Alert } from "react-native";
 import { Input } from "../components";
 import argonTheme from "../constants/Theme";
+import { ScrollView } from "react-native-gesture-handler";
+import { useState, useRef } from "react";
+import { useForm, useController } from 'react-hook-form';
+import { View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 const { height, width } = Dimensions.get('window');
 
-export default function WikiShare() {
+/** A share page which allows users to upload wiki pages to database */
+export default function WikiShare(props) {
+  return (<HookFormImplementation {...props} />)
+}
+
+/**
+ * Uses react-hook-form to implement the Share Page
+ * Link: https://react-hook-form.com/
+ */
+function HookFormImplementation(props) {
+  const { control, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    Alert.alert(
+      "Submitted Successfully",
+      JSON.stringify(data),
+    )
+  };
+
   const Heading = ({title="Title"}) => (<>
     <Text style={styles.heading}>
       {title}
     </Text>
   </>)
 
-  const InputBox = ({placeholder="input here..."}) => (<>
-    <Input
-      right
-      color="black"
-      style={styles.search}
-      placeholder={placeholder}
-      placeholderTextColor={'#8898AA'}
-      iconContent={null}
-    >
-    </Input>
-  </>)
+  const InputBox = ({
+    name,
+    placeholder=name,
+    control=null,
+  }) => {
+    const {field} = useController({
+      control,
+      defaultValue: "",
+      name,
+    })
+
+    return (<>
+      <Input
+        value={field.value}
+        onChangeText={field.onChange}
+        control={control}
+        // Styles
+        right
+        color="black"
+        style={styles.search}
+        placeholder={placeholder}
+        placeholderTextColor={'#8898AA'}
+        iconContent={null}
+      />
+    </>);
+  }
 
   return (
     <DevStatus status="developing">
       <Block>
-        <Heading title="Thumbnail" />
-        <InputBox placeholder="URL of the picture" />
-        <Heading title="Description" />
-        <InputBox placeholder="description" />
-        <Heading title="Location" />
-        <InputBox placeholder="location" />
-        <Heading title="Opening Hours" />
-        <InputBox placeholder="opening Hours" />
-        <Heading title="Map" />
-        <InputBox placeholder="URL of the map" />
-        <Heading title="External Website" />
-        <InputBox placeholder="URL of the website" />
-        <Button
-          title="submit"
-          onPress={() => {
-            Alert.alert(
-              "Submitted Successfully", 
-              "Submitted Wiki will be visible after being censored.")
-          }}
-        />
+        <ScrollView>
+          <Heading title="Thumbnail" />
+          <InputBox name="photo" placeholder="URL of the picture" control={control} />
+          <Heading title="Description" />
+          <InputBox name="description" control={control} />
+          <Heading title="Location" />
+          <InputBox name="location" control={control}/>
+          <Heading title="Opening Hours" />
+          <InputBox name="openingHours" placeholder="opening hours" control={control} />
+          <Heading title="Map" />
+          <InputBox name="map" placeholder="URL of the map" control={control} />
+          <Heading title="External Website" />
+          <InputBox name="url" placeholder="URL of the website" control={control} />
+          <Button
+            title="submit"
+            onPress={handleSubmit(onSubmit)}
+          />
+        </ScrollView>
       </Block>
     </DevStatus>
   );
