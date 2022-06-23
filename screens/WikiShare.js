c0cf15abd4080243fcf405bddb304e31301e5297
JarvisYu <JarvisYu_Pro@outlook.com>
2022-06-23 20:00:56 +0100

    Makes share inputBox diff height

--- screens/WikiShare.js
+++ screens/WikiShare.js
@@ -136,6 +136,7 @@ function HookFormImplementation(props) {
     name,
     placeholder=name,
     control=null,
+    height=48,
   }) => {
     const {field} = useController({
       control,
@@ -152,10 +153,11 @@ function HookFormImplementation(props) {
         autoCapitalize="none"
         right
         color="black"
-        style={styles.search}
+        style={[styles.search, {height}]}
         placeholder={placeholder}
         placeholderTextColor={'#8898AA'}
         iconContent={null}
+        multiline={true}
       />
     </>);
   }
@@ -170,9 +172,9 @@ function HookFormImplementation(props) {
           <Heading title="Photo" />
           <InputBox name="url" placeholder="URL of the picture" control={control} />
           <Heading title="Description" />
-          <InputBox name="description" control={control} />
+          <InputBox name="description" control={control} height={128} />
           <Heading title="Location" />
-          <InputBox name="location" control={control}/>
+          <InputBox name="location" control={control} height={64}/>
           <Heading title="Opening Hours" />
           <InputBox name="openingHour" placeholder="opening hours" control={control} />
           <Heading title="Map" />
@@ -180,7 +182,7 @@ function HookFormImplementation(props) {
           <Heading title="External Website" />
           <InputBox name="link" placeholder="URL of the website" control={control} />
           <Heading title="Tags (seperate tags by a single space)" />
-          <InputBox name="raw_tags" placeholder="example_tags sherfield_building relax" control={control} />
+          <InputBox name="raw_tags" placeholder="example_tags sherfield_building relax" control={control} height={64} />
           <View style={styles.buttonContainer}>
             <View style={styles.buttonWrapper}>
               <Button
